9f5a284a947f05218fdcc26d19df585b66ef3bdc
Debasish <luckypadhy011@gmail.com>
2023-04-10 15:36:29 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def spinup_data_ingestion(self):
+        logging.info('Data ingestion started.')
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Dataset fetch completed.')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
+            logging.info('Dataset is saved. Preparing train and test data...')
+
+            train, test = train_test_split(df, test_size = 0.2, random_state=4)
+            logging.info('Train and Test datasets are prepared.')
+
+            train.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            logging.info('Train and Test datasets are saved.')
+
+            logging.info('Data Ingestion process completed successfully.')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.spinup_data_ingestion()
